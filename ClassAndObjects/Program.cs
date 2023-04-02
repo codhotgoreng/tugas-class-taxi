45b9b669efa810dc04508bf26a83b88faeae7fb1
codhotgoreng <naafi@students.amikom.ac.id>
2023-04-03 00:00:37 +0700

    inisialisasi project

---
+++ ClassAndObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Object Taxi
+            Taxi taxi = new Taxi();
+
+            // Pengesetan Nilai Properties
+            Console.WriteLine("Masukkan Nama Driver: ");
+            taxi.DriverName = Console.ReadLine();
+            taxi.OnDuty = true;
+            Console.WriteLine("Masukkan Jumlah Penumpang: ");
+            taxi.NumPassenger = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            // Pemanggilan Method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAndObjects/Taxi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    class Taxi
+    { 
+        // Properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("TaxiInfo : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("PickUpPassenger : Yes", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("PickUpPassenger : No", OnDuty);
+            }
+
+            Console.WriteLine("DropOffPassenger : {0}", NumPassenger );
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Jono sedang menjemput penumpang");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Jono selesai mengantar penumpang");
+        }
+    }
+}
